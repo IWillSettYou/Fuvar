b353c01c49da1872e3051dbdb82780c885e3f536
iwillsettyou <bmate20050911@gmail.com>
2024-11-04 12:06:18 +0100

    Done

---
+++ Fuvar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuvar
+{
+    public class Fuvar
+    {
+        public int TaxiId { get; private set; }
+        public DateTime Indulas { get; private set; }
+        public int Ido { get; private set; }
+        public double Tavolsag { get; private set; }
+        public double Viteldij { get; private set; }
+        public double Borravalo { get; private set; }
+        public string FizetesModja { get; private set; }
+
+        public Fuvar(int taxiId, DateTime indulas, int ido, double tavolsag, double viteldij, double borravalo, string fizetesModja)
+        {
+            TaxiId = taxiId;
+            Indulas = indulas;
+            Ido = ido;
+            Tavolsag = tavolsag;
+            Viteldij = viteldij;
+            Borravalo = borravalo;
+            FizetesModja = fizetesModja;
+        }
+
+        public static List<Fuvar> LoadFromCsv(string fileName)
+        {
+            var fuvarok = new List<Fuvar>();
+            var lines = File.ReadAllLines(fileName);
+            for (int i = 1; i < lines.Length; i++) 
+            {
+                var fields = lines[i].Split(';');
+                var fuvar = new Fuvar(
+                    int.Parse(fields[0]),
+                    DateTime.ParseExact(fields[1], "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture),
+                    int.Parse(fields[2]),
+                    double.Parse(fields[3], CultureInfo.InvariantCulture),
+                    double.Parse(fields[4], CultureInfo.InvariantCulture),
+                    double.Parse(fields[5], CultureInfo.InvariantCulture),
+                    fields[6]
+                );
+                fuvarok.Add(fuvar);
+            }
+            return fuvarok;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var fuvarok = Fuvar.LoadFromCsv("../../../fuvar.csv");
+
+            // Feladat: 3
+            Console.WriteLine("3. feladat: " + fuvarok.Count + " fuvar");
+
+            // Feladat: 4
+            var taxi6185Rides = fuvarok.Where(f => f.TaxiId == 6185);
+            double totalRevenue = taxi6185Rides.Sum(f => f.Viteldij + f.Borravalo);
+            Console.WriteLine($"4. feladat: {taxi6185Rides.Count()} fuvar alatt: {totalRevenue}$");
+            // Feladat: 5
+            Console.WriteLine("5. feladat:");
+            var paymentMethods = fuvarok
+                .GroupBy(f => f.FizetesModja)
+                .ToDictionary(g => g.Key, g => g.Count());
+            foreach (var method in paymentMethods)
+            {
+                Console.WriteLine($"{method.Key}: {method.Value} fuvar");
+            }
+
+            // Feladat: 6
+            double totalDistanceMiles = fuvarok.Sum(f => f.Tavolsag);
+            double totalDistanceKm = totalDistanceMiles * 1.6;
+            Console.WriteLine($"6. feladat: {totalDistanceKm:F2} km");
+
+            // Feladat: 7
+            var longestRide = fuvarok.OrderByDescending(f => f.Ido).First();
+            Console.WriteLine("7. feladat: Leghosszabb fuvar:");
+            Console.WriteLine($"Fuvar hossza: {longestRide.Ido} másodperc");
+            Console.WriteLine($"Taxi azonosító: {longestRide.TaxiId}");
+            Console.WriteLine($"Megtett távolság: {longestRide.Tavolsag * 1.6:F1} km");
+            Console.WriteLine($"Viteldíj: {longestRide.Viteldij}$");
+
+            // Feladat: 8
+            var faultyRides = fuvarok
+                .Where(f => f.Ido > 0 && f.Viteldij > 0 && f.Tavolsag == 0)
+                .OrderBy(f => f.Indulas)
+                .Select(f => $"{f.TaxiId};{f.Indulas:yyyy-MM-dd HH:mm:ss};{f.Ido};{f.Tavolsag};{f.Viteldij};{f.Borravalo};{f.FizetesModja}")
+                .ToList();
+            File.WriteAllLines("hibak.txt", new[] { "taxi_id;indulas_idopontja;idotartam;tavolsag;viteldij;borravalo;fizetes_modja" }.Concat(faultyRides));
+            Console.WriteLine("8. feladat: hibak.txt");
+
         }
     }
 }
